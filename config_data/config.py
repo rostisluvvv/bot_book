7ad5b4720a42cc44629ba8e3824322fc4406b514
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-01-15 13:49:40 +0300

    файл config и bot заполнены

---
+++ config_data/config.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from environs import Env
+
+
+@dataclass
+class TgBot:
+    token: str              #bot tokeb
+    admin_id: list[int]     #admin id
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+
+
+def load_config(path: str | None = None) -> Config:
+    env = Env()
+    env.read_env(path)
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN'),
+                               admin_id=list(map(int, env.list('ADMIN_ID')))))
