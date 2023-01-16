9e24deaaa93d330b797b7b7167e12b020696ee19
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-01-16 13:28:40 +0300

    функция prepare_book выдает содержимое файла book.txt

--- services/file_handling.py
+++ services/file_handling.py
@@ -1,13 +1,13 @@
 import re
 
 BOOK_PATH: str = "book/book.txt"
-PAGE_SIZE: int = 1050
+PAGE_SIZE: int = 100
 
 book: dict[int, str] = {}
 
 
 # func returns str with TXT and her size
-def _get_part_text(text: str, start: int, size: int):
+def _get_part_text(text: str, start: int, size: int) -> tuple[str, int]:
 
     text_with_punctuation_marks = text[start:start + size]
 
@@ -17,21 +17,24 @@ def _get_part_text(text: str, start: int, size: int):
 
     del_word = text_on_page[len(text_on_page)-1:len(text_on_page)]
     del_word = ''.join(str(x) for x in del_word)
-
     correct_text = text_with_punctuation_marks.removesuffix(del_word)
 
-    return (correct_text, len(correct_text) )
+    return correct_text, len(correct_text)
 
 
-
-
-# func create dict of book
+# Функция, формирующая словарь книги
 def prepare_book(path: str) -> None:
-    pass
+
+    with open('C:\Dev\stepik_AIOgram\\6bot_book\\book\\book.txt', "r", encoding="utf-8") as file:
+        txt = file.read()
+
+        txt = re.sub(r"\s", " ", txt)
+
+        out_txt = re.split(r"[()]", txt)
+
+        return out_txt
 
 
-prepare_book(BOOK_PATH)
 
-text = 'Раз. Два. Три. Четыре. Пять. Прием!'
-
-print(*_get_part_text(text, 5, 9), sep='\n')
+# Вызов функции prepare_book для подготовки книги из текстового файла
+print(prepare_book(BOOK_PATH))
