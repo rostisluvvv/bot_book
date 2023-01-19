2040b9c5cd8e5036cad60e20235898084f227848
Ðîîñòèñëà Óñïàñêèé <ruspasskiy2002@gmail.com>
2023-01-19 14:43:06 +0300

    создана клавиатура с пагинацией

---
+++ keyboards/pagination_kb.py
@@ -0,0 +1,13 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+from lexicon.lexicon import LEXICON
+
+
+# фнткция генерирующая клавиатуру для страниц книги
+def create_pagination_keyboard(*buttons: str) -> InlineKeyboardMarkup:
+    pagination_kb: InlineKeyboardMarkup = InlineKeyboardMarkup()
+    pagination_kb.row(*[InlineKeyboardButton(
+        LEXICON[button] if button in LEXICON
+        else button, callback_data=button)
+        for button in buttons])
+    return pagination_kb
